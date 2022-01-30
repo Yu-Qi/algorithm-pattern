457b3e34075a470eed0cc6fea86a46021298a685
YQ CHEN <alan140720@gmail.com>
2022-01-31 01:13:27 +0800

    [01/31] 110 - isBalanced, 將運算抽出max function

--- src/110_isBalanced/algo.go
+++ src/110_isBalanced/algo.go
@@ -5,6 +5,13 @@ type TreeNode struct {
 	Left *TreeNode
 	Right *TreeNode
 }
+func max(a int, b int)int{
+	if a>=b{
+		return a
+	} else{
+		return b
+	}
+}
 func Abs(num int)int{
 	if num >=0{
 		return num
@@ -20,16 +27,12 @@ func GetDepth(root *TreeNode)(int,bool){
 	depth_right, balanced_right :=  GetDepth(root.Right)
 
 	fmt.Printf("value=%d,depth_left=%d,balanced_left=%t,depth_right=%d,balanced_right=%t\n",root.Val,depth_left,balanced_left,depth_right,balanced_right)
-	if !balanced_lef|| !balanced_right{
+	if !balanced_left|| !balanced_right{
 		return -1,false
 	} else if Abs(depth_left-depth_right) >=2 {
 		return -1,false
 	} else{
-		if depth_left >= depth_right{
-			return depth_left+1,true
-		} else{
-			return depth_right+1,true
-		}
+		return max(depth_left+1,depth_right+1),true
 	}
 }
 
