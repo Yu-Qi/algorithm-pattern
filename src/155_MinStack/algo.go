ffab58288415ce5b1805a47738386af5eb6198ab
YQ CHEN <alan140720@gmail.com>
2022-02-15 18:18:19 +0800

    [02/20] 實際02/15 155 - MinStack, 從最後塞入,簡化min stack

--- src/155_MinStack/algo.go
+++ src/155_MinStack/algo.go
@@ -4,41 +4,28 @@ type MinStack struct {
     stack []int
 	min []int
 }
-
-
 func Constructor() MinStack {
     return MinStack{
 		stack: make([]int, 0),
 		min: make([]int, 0),
 	}
 }
-func (this *MinStack) remove(val int){
-	idx :=0
-	for ;i<len(this.min);i++{
-		if this.min[idx] == val{
-			this.min = append(this.min[:idx],this.min[idx+1:]...)
-			return
-		}
-}
-func (this *MinStack) insert(val int){
-	idx :=0
-	for ;i<len(this.min);i++{
-		if val <= this.min[i]{
-			break
-		}
-	}
-	tmp := append(this.min[:idx],val)
-	this.min = append(tmp, this.min[idx:])
-}
+
 func (this *MinStack) Push(val int)  {
-    this.stack = append([]int{val}, this.stack...)
-	insert(val)
+    this.stack = append(this.stack, val)
+
+	// 
+    min := this.GetMin()
+	if min < val{
+		this.min = append(this.min, min)
+	} else{
+		this.min = append(this.min, val)
+	}
 }
 
 func (this *MinStack) Pop()  {
-	item := this.stack[0]
-    this.stack = this.stack[1:]
-	remove(item)
+    this.stack = this.stack[:len(this.stack)-1]
+    this.min = this.min[:len(this.min)-1]
 }
 
 
@@ -46,12 +33,12 @@ func (this *MinStack) Top() int {
 	if len(this.stack) == 0{
 		return 0
 	}
-    return this.stack[0]
+    return this.stack[len(this.stack)-1]
 }
 
 func (this *MinStack) GetMin() int {
-	if len(this.stack) == 0{
+	if len(this.min) == 0{
 		return 1 <<31
 	}
-    return this.min[0]
+    return this.min[len(this.min)-1]
 }
