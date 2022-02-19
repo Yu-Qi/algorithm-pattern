7d906ca4fa47d57f4d1e17730ebce969180f6d21
Yu-Qi <alan140720@gmail.com>
2022-02-19 12:20:56 +0800

    NOT YET[03/13] 實際02/19, 154-findMin,HARD

---
+++ src/154_findMin/algo.go
@@ -0,0 +1,19 @@
+package algo
+
+func findMin(nums []int) int {
+	start:=0
+	end:= len(nums)-1
+	for start+1<end{
+		middle := (start+end)/2
+		if nums[middle] > nums[end]{
+			start = middle
+        } else{
+            end = middle
+        }
+	}
+    if nums[start]<nums[end]{
+        return nums[start]
+    }else{
+        return nums[end]
+    }
+}
