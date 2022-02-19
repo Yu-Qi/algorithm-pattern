dd27fab521b0cd7122ca9940b1ef8176b823876b
Yu-Qi <alan140720@gmail.com>
2022-02-19 11:30:51 +0800

    [03/11] 實際02/19, 278-firstBadVersion,模板3讚讚

--- src/278_firstBadVersion/algo.go
+++ src/278_firstBadVersion/algo.go
@@ -1,9 +1,19 @@
 package algo
 
- func firstBadVersion(n int) int {
-	start :=0
-	end := n-1
+func firstBadVersion(n int) int {
+	start :=1
+	end := n
 	for start+1<end{
-		middle:=
+		middle := (start+end)/2
+		if isBadVersion(middle){
+			end = middle
+		} else{
+			start = middle
+		}
+	}
+	if isBadVersion(start){
+		return start
+	} else{
+		return end
 	}
 }
